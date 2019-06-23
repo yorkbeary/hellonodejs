04d9fbb9c72be209e45e1fd87d419f66cc5ac2b2
Zijiang Yang <yorkbeary@gmail.com>
2019-06-23 21:56:39 +0800

    expressjs

--- index.js
+++ index.js
@@ -1,11 +1,6 @@
-const http = require('http');
-
-const server = http.createServer((request, response) => {
-    response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("Hello server!");
-});
-
-const port = process.env.PORT || 3000;
-server.listen(port);
-
-console.log("Server running at http://localhost:%d", port);
+const express = require('express');
+const app = express();
+app.get('/', (req, res) => {
+    res.send('<h1>hello</h1>')
+})
+app.listen(3000);
