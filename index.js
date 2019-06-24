e66e84cf36f730ed109ec7962eb820b4165b831d
Zijiang Yang <yorkbeary@gmail.com>
2019-06-24 11:29:09 +0800

    body-parser

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+
 const app = express();
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
 app.get('/', (req, res) => {
+    console.log(req.query);
     res.send('<h1>hello</h1>')
 })
 app.listen(3000);
